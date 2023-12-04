00a4f3ad3da66a248244dbdd3c5196cf6a2b85e6
Nick Latcham <nick.latcham@proton.me>
2023-12-03 21:17:37 -0600

    Starting day two

---
+++ src/main/java/org/godDamnElves/dayTwo/PartOne.java
@@ -0,0 +1,16 @@
+package org.godDamnElves.dayTwo;
+
+import java.util.Map;
+
+public class PartOne {
+
+    private static final Map<String, Integer> bagOfCubes = Map.of(
+            "red", 12,
+            "green", 13,
+            "blue", 14
+    );
+
+    // INCOMPLETE
+
+
+}
---
+++ src/main/java/org/godDamnElves/dayTwo/PartTwo.java
@@ -0,0 +1,4 @@
+package org.godDamnElves.dayTwo;
+
+public class PartTwo {
+}
